551f1aacd9a6b7c95da4b06439da7005779a61d7
otodtt <dtenevoto@mail.bg>
2017-12-23 03:11:04 +0200

    22.12.2017 -2

--- src/app/shared/side-nav/animations.ts
+++ src/app/shared/side-nav/animations.ts
@@ -1,6 +1,4 @@
-// import { trigger, state, style, transition, animate } from '@angular/animations';
-import { trigger, state, style, transition, animate } from '@angular/core';
-// import { animate } from '@angular/animations/src/animation_metadata';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 
 // export const MenuButtonAnimation = trigger('menuState', [
@@ -10,16 +8,125 @@ import { trigger, state, style, transition, animate } from '@angular/core';
 //     transition('show => hide', animate('350ms ease-in'))
 // ]);
 
+
 export const MenuButtonAnimation = trigger('rotatedState', [
     state('default', style({
-        transform: 'translateX(0)'
+        transform: 'translateX(0)',
+        opacity: 1,
+        position: 'absolute'
     })),
-    state('rotated', style({
-        transform: 'translateX(-100%)',
+    state('hidden', style({
+        transform: 'translateX(-120%)',
         opacity: 0,
-        // display: 'none'
+        // position: 'absolute',
+        display: 'none'
     })),
-    transition('rotated => default', animate('400ms ease-out')),
-    transition('default => rotated', animate('400ms ease-in'))
+    // ==========
+    state('default-sub', style({
+        transform: 'translateX(120%)',
+        opacity: 0,
+        // position: 'absolute'
+        display: 'none'
+    })),
+    state('shown', style({
+        transform: 'translateX(0)',
+        opacity: 1,
+        position: 'absolute'
+    })),
+    transition('default => hidden', animate('300ms', keyframes([
+        style({
+            transform: 'translateX(0)',
+            offset: 0,
+            opacity: 1,
+        }),
+        style({
+            transform: 'translateX(-30%)',
+            offset: .3,
+            opacity: .6,
+        }),
+        style({
+            transform: 'translateX(-60%)',
+            offset: .6,
+            opacity: .3,
+        }),
+        style({
+            transform: 'translateX(-120%)',
+            offset: 1,
+            opacity: 0,
+        }),
+    ]))),
+    // ===========
+
+    transition('default-sub => shown', animate('300ms', keyframes([
+        style({
+            transform: 'translateX(120%)',
+            offset: 0,
+            opacity: 0,
+        }),
+        style({
+            transform: 'translateX(60%)',
+            offset: .3,
+            opacity: .3,
+        }),
+        style({
+            transform: 'translateX(30%)',
+            offset: .6,
+            opacity: .6,
+        }),
+        style({
+            transform: 'translateX(0)',
+            offset: 1,
+            opacity: 1,
+        }),
+    ]))),
+
+
+    /*
+        RETURN DEFAULT
+    */
+    transition('hidden => default', animate('300ms', keyframes([
+        style({
+            transform: 'translateX(-120%)',
+            offset: 0,
+            opacity: 0,
+        }),
+        style({
+            transform: 'translateX(-60%)',
+            offset: .3,
+            opacity: .3,
+        }),
+        style({
+            transform: 'translateX(-30%)',
+            offset: .6,
+            opacity: .6,
+        }),
+        style({
+            transform: 'translateX(0)',
+            offset: 1,
+            opacity: 1,
+        }),
+    ]))),
+    transition('shown => default-sub', animate('300ms', keyframes([
+        style({
+            transform: 'translateX(0)',
+            offset: 0,
+            opacity: 1,
+        }),
+        style({
+            transform: 'translateX(30%)',
+            offset: .3,
+            opacity: .6,
+        }),
+        style({
+            transform: 'translateX(60%)',
+            offset: .6,
+            opacity: .3,
+        }),
+        style({
+            transform: 'translateX(120%)',
+            offset: 1,
+            opacity: 0,
+        }),
+    ])))
 ]);
 
--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -7,44 +7,70 @@ import { MenuButtonAnimation } from './animations';
     selector: 'prz-side-nav',
     templateUrl: './side-nav.component.html',
     styleUrls: ['./side-nav.component.scss'],
-    animations: [MenuButtonAnimation]
+    animations: [ MenuButtonAnimation ]
 })
 export class SideNavComponent implements OnInit {
-    state = 'default';
+    public state = 'default';
+    public stateSub = 'default-sub';
+
+    // public return = 'return-default';
+    // public returnMenu = '';
+    // public returnSub = 'default-sub';
+    public isClicked = false;
+
+    // public done: number;
+    // public id: number;
 
 
 
 
     public pages = pages;
+    public subPages: any;
     public fillerShow = null;
 
     @Output() navClose = new EventEmitter<boolean>();
     constructor() { }
 
     ngOnInit() {
+        // console.log(this.return);
     }
 
-    showFiller(index: number) {
-        if (this.fillerShow === index) {
-            this.fillerShow = null;
-        } else {
-            this.fillerShow = index;
-        }
-        // console.log(event);
-    }
+    // showFiller(index: number) {
+    //     if (this.fillerShow === index) {
+    //         this.fillerShow = null;
+    //     } else {
+    //         this.fillerShow = index;
+    //     }
+    //     // console.log(event);
+    // }
 
     onCloseMenu() {
         this.navClose.emit(false);
     }
 
-    rotate(index) {
-        this.state = (this.state === 'default' ? 'rotated' : 'default');
-        // setTimeout(() => {
-        //     this.showFiller(index);
-        // }, 500);
+    rotate(index?) {
+        this.state = (this.state === 'default' ? 'hidden' : 'default');
+        this.stateSub = (this.stateSub === 'default-sub' ? 'shown' : 'default-sub');
+        // this.returnSub = 'default-sub';
+        this.isClicked = true;
+        this.subPages = this.pages[index].subPage;
+        // console.log(this.returnMenu + ' ---> up <---- ' + this.state);
+        // console.log(this.stateSub + ' ---> up <---- ' );
+    }
+
+    returnDefaultMenu() {
+        this.state = (this.state === 'default' ? 'hidden' : 'default');
+        this.stateSub = (this.stateSub === 'default-sub' ? 'shown' : 'default-sub');
+        // this.returnMenu = (this.returnMenu === 'return-default' ? 'hide' : 'return-default');
+        // this.returnSub = (this.returnSub === 'default-sub' ? 'hide' : 'default-sub');
+        // this.state = 'default';
+        this.isClicked = false;
+        // console.log(this.returnMenu + ' ---> down <---- ' + this.state);
+        // console.log(this.returnSub + ' ---> down <---- ');
     }
     animationDone(event, index) {
-        console.log(index);
+        // this.done = 1;
+        // console.log('done');
     }
 
 }
