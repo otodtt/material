f0815640121ab36ecb3628041d6793a10ef977c8
otodtt <dtenevoto@mail.bg>
2018-01-15 17:05:48 +0200

    15,01,2018 -1

--- src/app/practices/practices/pages/beans/glycine/glycine.component.ts
+++ src/app/practices/practices/pages/beans/glycine/glycine.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
-  selector: 'prz-glycine',
-  templateUrl: './glycine.component.html',
-  styleUrls: ['./glycine.component.scss']
+    templateUrl: './glycine.component.html',
+    styleUrls: ['../../pages.scss']
 })
-export class GlycineComponent implements OnInit {
+export class GlycineComponent implements OnInit, OnDestroy {
+    private title = 'ДРЗП - Соя';
+    private description = 'Добра Растителнозащитна Пракатика при соя. Борба с болести, неприятели и плевели при соята.';
+    private keywords = 'соя, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-  constructor() { }
+    breadcrumbName = 'Соя';
 
-  ngOnInit() {
-  }
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
 
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
+    }
+
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('glycine')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
