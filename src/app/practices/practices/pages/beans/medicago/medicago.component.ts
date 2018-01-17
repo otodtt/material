754226a2f1fed041cfbd40fe520fb8245c28d274
otodtt <dtenevoto@mail.bg>
2018-01-17 17:22:05 +0200

    17.01.2018 -1

--- src/app/practices/practices/pages/beans/medicago/medicago.component.ts
+++ src/app/practices/practices/pages/beans/medicago/medicago.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
-  selector: 'prz-medicago',
   templateUrl: './medicago.component.html',
-  styleUrls: ['./medicago.component.scss']
+  styleUrls: ['../../pages.scss']
 })
-export class MedicagoComponent implements OnInit {
+export class MedicagoComponent implements OnInit, OnDestroy  {
+    private title = 'ДРЗП - Люцерна';
+    private description = 'Добра Растителнозащитна Пракатика при люцерна. Борба с болести, неприятели и плевели при люцерната.';
+    private keywords = 'люцерна, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-  constructor() { }
+    breadcrumbName = 'Люцерна';
 
-  ngOnInit() {
-  }
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
 
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
+    }
+
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('medicago')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
--- src/app/practices/practices/pages/technical/nicotiana/nicotiana.component.ts
+++ src/app/practices/practices/pages/technical/nicotiana/nicotiana.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
-  selector: 'prz-nicotiana',
-  templateUrl: './nicotiana.component.html',
-  styleUrls: ['./nicotiana.component.scss']
+    templateUrl: './nicotiana.component.html',
+    styleUrls: ['../../pages.scss']
 })
-export class NicotianaComponent implements OnInit {
+export class NicotianaComponent implements OnInit, OnDestroy {
+    private title = 'ДРЗП - Тютюн';
+    private description = 'Добра Растителнозащитна Пракатика при тютюн. Борба с болести, неприятели и плевели при тютюн.';
+    private keywords = 'тютюн, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-  constructor() { }
+    breadcrumbName = 'Тютюн';
 
-  ngOnInit() {
-  }
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
+
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
+    }
+
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('nicotiana')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 
 }
