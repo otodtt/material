443493548420add517db55c65926a633230a0e72
otodtt <dtenevoto@mail.bg>
2018-01-18 16:59:19 +0200

    18.01.2018 -1

--- src/app/practices/practices/pages/technical/gossypium/gossypium.component.ts
+++ src/app/practices/practices/pages/technical/gossypium/gossypium.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
-  selector: 'prz-gossypium',
-  templateUrl: './gossypium.component.html',
-  styleUrls: ['./gossypium.component.scss']
+    templateUrl: './gossypium.component.html',
+    styleUrls: ['../../pages.scss']
 })
-export class GossypiumComponent implements OnInit {
+export class GossypiumComponent implements OnInit, OnDestroy {
+    private title = 'ДРЗП - Памук';
+    private description = 'Добра Растителнозащитна Пракатика при памук. Борба с болести, неприятели и плевели при памук.';
+    private keywords = 'памук, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-  constructor() { }
+    breadcrumbName = 'Памук';
 
-  ngOnInit() {
-  }
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
+
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
+    }
+
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('gossypium')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
+
 
 }
