8c2f9a85c2ba39abf4da69199547932ceb8b3a48
otodtt <dtenevoto@mail.bg>
2018-01-21 12:06:48 +0200

    20.01.2018 -end tehnical

--- src/app/practices/practices/pages/technical/arachis/arachis.component.ts
+++ src/app/practices/practices/pages/technical/arachis/arachis.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
-  selector: 'prz-arachis',
-  templateUrl: './arachis.component.html',
-  styleUrls: ['./arachis.component.scss']
+    selector: 'prz-arachis',
+    templateUrl: './arachis.component.html',
+    styleUrls: ['../../pages.scss']
 })
-export class ArachisComponent implements OnInit {
+export class ArachisComponent implements OnInit, OnDestroy {
+    private title = 'ДРЗП - Фъстъци';
+    private description = 'Добра Растителнозащитна Пракатика при фъстъци. Борба с болести, неприятели и плевели при фъстъците.';
+    private keywords = 'фъстъци, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-  constructor() { }
+    breadcrumbName = 'Фъстъци';
 
-  ngOnInit() {
-  }
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
 
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
+    }
+
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('arachis')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
--- src/app/practices/practices/pages/technical/helianthus/helianthus.component.ts
+++ src/app/practices/practices/pages/technical/helianthus/helianthus.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
-  selector: 'prz-helianthus',
-  templateUrl: './helianthus.component.html',
-  styleUrls: ['./helianthus.component.scss']
+    templateUrl: './helianthus.component.html',
+    styleUrls: ['../../pages.scss']
 })
-export class HelianthusComponent implements OnInit {
+export class HelianthusComponent implements OnInit, OnDestroy {
+    private title = 'ДРЗП - Слънчоглед';
+    private description = 'Добра Растителнозащитна Пракатика при слънчоглед. Борба с болести, неприятели и плевели при слънчогледа.';
+    private keywords = 'слънчоглед, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-  constructor() { }
+    breadcrumbName = 'Слънчоглед';
 
-  ngOnInit() {
-  }
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
 
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
+    }
+
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('helianthus')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
