6e168fbc32c7cc2e9aecc752dc80a272a78a8cc5
otodtt <dtenevoto@mail.bg>
2018-02-01 17:05:11 +0200

    01.02.2018 -1vg

--- src/app/practices/practices/pages/vegetables/cabbage/cabbage.component.ts
+++ src/app/practices/practices/pages/vegetables/cabbage/cabbage.component.ts
@@ -1,14 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
     templateUrl: './cabbage.component.html',
-    styleUrls: ['../../pages.scss', '../../../shared/dialog/dialog.component.scss']
+    styleUrls: ['../../pages.scss']
 })
-export class CabbageComponent implements OnInit {
+export class CabbageComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    private title = 'ДРЗП - Зелеви култури';
+    private description =   'Добра Растителнозащитна Пракатика при главесто зеле, карфиол, алабаш, ряпа. ' +
+                            'Добра Растителнозащитна Пракатика при зелеви култури. ' +
+                            ' Борба с болести, неприятели и плевели при главесто зеле, карфиол, алабаш, ряпа.';
+    private keywords = 'главесто зеле, карфиол, алабаш, ряпа, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-    ngOnInit() {
+    breadcrumbName = 'Зелеви култури';
+
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
+
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
     }
 
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('cabbage')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
--- src/app/practices/practices/pages/vegetables/pumpkin/pumpkin.component.ts
+++ src/app/practices/practices/pages/vegetables/pumpkin/pumpkin.component.ts
@@ -1,14 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
     templateUrl: './pumpkin.component.html',
-    styleUrls: ['../../pages.scss', '../../../shared/dialog/dialog.component.scss']
+    styleUrls: ['../../pages.scss']
 })
-export class PumpkinComponent implements OnInit {
+export class PumpkinComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    private title = 'ДРЗП - Тиквови култури';
+    private description =   'Добра Растителнозащитна Пракатика при краставици, тиквички, тикви, дини, пъпеши. ' +
+                            'Добра Растителнозащитна Пракатика при тиквови култури. ' +
+                            ' Борба с болести, неприятели и плевели при краставици, тиквички, тикви, дини, пъпеши.';
+    private keywords = 'краставици, тиквички, тикви, дини, пъпеши, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-    ngOnInit() {
+    breadcrumbName = 'Тиквови култури';
+
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
+
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
     }
 
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('pumpkin')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
