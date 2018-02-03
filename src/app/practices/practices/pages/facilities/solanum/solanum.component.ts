e660c8733cd3057cdec7a50745d2321b69ac4a7a
otodtt <dtenevoto@mail.bg>
2018-02-03 12:07:19 +0200

    02.02.2018 -2 solanum

--- src/app/practices/practices/pages/facilities/solanum/solanum.component.ts
+++ src/app/practices/practices/pages/facilities/solanum/solanum.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: './solanum.component.html',
-    styleUrls: ['../../pages.scss']
+    styleUrls: ['../../pages.scss', '../../../shared/dialog/dialog.component.scss']
 })
 export class SolanumComponent implements OnInit {
 
-      constructor() { }
+    constructor() { }
 
-      ngOnInit() {
-      }
+    ngOnInit() {
+    }
 
 }
