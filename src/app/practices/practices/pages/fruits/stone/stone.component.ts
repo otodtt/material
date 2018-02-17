64fa4a180fef4685b53682fbd20942511ec58aeb
otodtt <dtenevoto@mail.bg>
2018-02-17 16:36:19 +0200

    16.02.2018 -2strow

--- src/app/practices/practices/pages/fruits/stone/stone.component.ts
+++ src/app/practices/practices/pages/fruits/stone/stone.component.ts
@@ -1,14 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
     templateUrl: './stone.component.html',
-    styleUrls: ['../../pages.scss', '../../../shared/dialog/dialog.component.scss']
+    styleUrls: ['../../pages.scss']
 })
-export class StoneComponent implements OnInit {
+export class StoneComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    private title = 'ДРЗП - Костилкови овощни видове';
+    private description =   'Добра Растителнозащитна Пракатика при слива, череша, вишна, праскова и кайсия. ' +
+                            'Добра Растителнозащитна Пракатика при костилкови овощни видове. ' +
+                            ' Борба с болести, неприятели и плевели при слива, череша, вишна, праскова и кайсия.';
+    private keywords = 'слива, череша, вишна, праскова и кайсия, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-    ngOnInit() {
+    breadcrumbName = 'Костилкови';
+
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
+
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
     }
 
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('stone')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
--- src/app/practices/practices/pages/strawberry/nigrum/nigrum.component.ts
+++ src/app/practices/practices/pages/strawberry/nigrum/nigrum.component.ts
@@ -1,14 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
+
+import { SeoService } from '../../../../../common/services/SeoService';
+import { ChangeBreadcrumbService } from '../../../../../common/services/changeBreadcrumb.service';
+
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+
+import { Practice } from '../../../shared/models/practice.model';
+import { PracticesService } from '../../../shared/services/practices.services';
 
 @Component({
     templateUrl: './nigrum.component.html',
-    styleUrls: ['../../pages.scss', '../../../shared/dialog/dialog.component.scss']
+    styleUrls: ['../../pages.scss']
 })
-export class NigrumComponent implements OnInit {
+export class NigrumComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    private title = 'ДРЗП - Касис';
+    private description =   'Добра Растителнозащитна Пракатика при касис. ' +
+                            'Борба с болести, неприятели и плевели при касис.';
+    private keywords = 'касис, болести, неприятели, плевели, ПРЗ, ПИВ';
 
-    ngOnInit() {
+    breadcrumbName = 'Касис';
+
+    isLoaded = false;
+    practices: Practice[] = [];
+    subscription: Subscription;
+
+    constructor(
+        private seoService: SeoService,
+        private changeBreadcrumbService: ChangeBreadcrumbService,
+        public dialog: MatDialog,
+        private practicesService: PracticesService
+    ) {
+        this.seoService.addTitle(this.title);
+        this.seoService.setMeta(this.description, this.keywords);
     }
 
+    ngOnInit() {
+        this.changeBreadcrumbService.emitName(this.breadcrumbName);
+        this.subscription = this.practicesService.getPractices('nigrum')
+            .subscribe((practices: Practice[]) => {
+                this.practices = practices;
+                this.isLoaded = true;
+            });
+    }
+
+    openDialog(table: string) {
+        this.dialog.open(DialogComponent, {
+            data: { table: table}
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+          this.subscription.unsubscribe();
+        }
+    }
 }
